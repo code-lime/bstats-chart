5d36fd10520524b3486f54b637f0e571e2f08906
code-lime <demn2001@gmail.com>
2025-11-02 02:43:40 +0300

    offset first color

--- src/DotNet.GitHubAction/IndexedColor.cs
+++ src/DotNet.GitHubAction/IndexedColor.cs
@@ -7,9 +7,12 @@ namespace DotNet.GitHubAction;
 
 public static class IndexedColor
 {
+    private const float HUE_OFFSET = 204.2f;
+
     public static Rgba32 GetColor(int index, int total, float alpha = 1)
     {
-        var hsv = new Hsv((index * 360f / total) % 360f, 0.65f, 0.95f);
+        float hue = index * 360f + HUE_OFFSET;
+        var hsv = new Hsv(hue / total % 360f, 0.65f, 0.95f);
         return new Rgba32(new Vector4(ColorSpaceConverter.ToRgb(hsv).ToVector3(), alpha));
     }
 }
